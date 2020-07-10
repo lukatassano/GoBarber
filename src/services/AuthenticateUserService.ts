5042dce7d3e2854d98acc9aa99c8634f5a7fc072
Luka Tassano <lukatassano070@gmail.com>
2020-07-09 22:45:49 -0300

    add json web token and return token

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,8 +1,8 @@
 import { getRepository } from 'typeorm';
-// import { hash } from "bcryptjs";
+import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
 
 import User from '../models/User';
-import { compare } from 'bcryptjs';
 
 interface Request {
   email: string;
@@ -11,6 +11,7 @@ interface Request {
 
 interface Response {
   user: User;
+  token: string;
 }
 
 class AuthenticateUserService {
@@ -32,10 +33,14 @@ class AuthenticateUserService {
       throw new Error('Incorrect email/password combination.');
     }
 
-    // Usuário autenticado
+    const token = sign({}, 'b3c4ced8a1f531b3a3d032f72c92dd82', {
+      subject: user.id,
+      expiresIn: '1d',
+    });
 
     return {
       user,
+      token,
     };
   }
 }
